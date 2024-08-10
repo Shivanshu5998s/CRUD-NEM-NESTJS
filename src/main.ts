25a1127a0118379ec482163a4119bdc63b40f1ae
Shivanshu Chobey <shivanshuchobey5998@gmail.com>
2024-08-10 11:23:19 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for the frontend origin
+  app.enableCors({
+    origin: 'http://localhost:3001', // Replace with the URL of your frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
 
-
-// entry point file
+// Entry point file.
 // NESTJS for build scalable and fast api.
